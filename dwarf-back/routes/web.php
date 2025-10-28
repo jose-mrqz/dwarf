96e20fdd276c49072582fbbc17ecc12fc90b09de
jose-mrqz <josel.marquez.almeyda@gmail.com>
2025-10-28 00:55:17 -0500

    fix redirect url

--- dwarf-back/routes/web.php
+++ dwarf-back/routes/web.php
@@ -5,5 +5,5 @@ use App\Http\Controllers\UrlController;
 
 // set public cache on client for 24 hours
 Route::middleware('cache.headers:public;max_age=86400;etag')->group(function () {
-    Route::get('/{code}/redirect', [UrlController::class, 'redirect'])->name('urls.redirect');
+    Route::get('/urls/{code}/redirect', [UrlController::class, 'redirect'])->name('urls.redirect');
 });
